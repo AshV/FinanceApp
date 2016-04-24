5098d3233815a1d04b058cb8e3474ecb64fdc239
Ashish Vishwakarma <ashish6318@gmail.com>
2016-04-24 12:55:09 +0530

    Not in mood to Add message.

--- CustomScripts/Scripts.js
+++ CustomScripts/Scripts.js
@@ -28,6 +28,10 @@ financeApp.config(function ($routeProvider) {
             templateUrl: 'Pages/Client/UpdateClient.html',
             controller: 'UpdateClientController'
         })
+        .when('/ViewClient', {
+            templateUrl: 'Pages/Client/ViewClient.html',
+            controller: 'UpdateClientController'
+        })
         .when('/NewLoanProposal', {
             templateUrl: 'Pages/LoanProposal/NewProposal.html',
             controller: 'NewLoanProposalController'
@@ -135,12 +139,19 @@ financeApp.controller('AddNewEmployeeController', function ($scope, $firebaseObj
                 };
             })(f);
             reader.readAsBinaryString(f);
-        
-    }
+           }
 });
 
 financeApp.controller('UpdateEmployeeController', function ($scope, $firebaseObject, $location) {
     var rootRef = new Firebase(ROOTREF);
+    var CenterDdlRef = rootRef.child("Center");
+
+    CenterDdlRef.on("value", function (data) {
+        $scope.Centers = data.val();
+    }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+    });
+
     var recordRef = rootRef.child("Employee/" + $location.search().ID);
     recordRef.on("value", function (data) {
         $scope.Employee = data.val();
@@ -151,7 +162,10 @@ financeApp.controller('UpdateEmployeeController', function ($scope, $firebaseObj
     var imageRef = rootRef.child("EmployeePics/" + $location.search().ID);
     imageRef.on("value", function (data) {
         $scope.EmployeePic = data.val();
-        document.getElementById('base64').src = "data:image/png;base64, " + $scope.EmployeePic;
+        if ($scope.EmployeePic != null)
+            document.getElementById('base64').src = "data:image/png;base64, " + $scope.EmployeePic;
+        else
+            document.getElementById('base64').src = "data:image/png;base64, " + "/9j/4AAQSkZJRgABAQAAAQABAAD//gA7Q1JFQVRPUjogZ2QtanBlZyB2MS4wICh1c2luZyBJSkcgSlBFRyB2NjIpLCBxdWFsaXR5ID0gOTAK/9sAQwADAgIDAgIDAwMDBAMDBAUIBQUEBAUKBwcGCAwKDAwLCgsLDQ4SEA0OEQ4LCxAWEBETFBUVFQwPFxgWFBgSFBUU/9sAQwEDBAQFBAUJBQUJFA0LDRQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQU/8AAEQgAlgCWAwEiAAIRAQMRAf/EAB8AAAEFAQEBAQEBAAAAAAAAAAABAgMEBQYHCAkKC//EALUQAAIBAwMCBAMFBQQEAAABfQECAwAEEQUSITFBBhNRYQcicRQygZGhCCNCscEVUtHwJDNicoIJChYXGBkaJSYnKCkqNDU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6g4SFhoeIiYqSk5SVlpeYmZqio6Slpqeoqaqys7S1tre4ubrCw8TFxsfIycrS09TV1tfY2drh4uPk5ebn6Onq8fLz9PX29/j5+v/EAB8BAAMBAQEBAQEBAQEAAAAAAAABAgMEBQYHCAkKC//EALURAAIBAgQEAwQHBQQEAAECdwABAgMRBAUhMQYSQVEHYXETIjKBCBRCkaGxwQkjM1LwFWJy0QoWJDThJfEXGBkaJicoKSo1Njc4OTpDREVGR0hJSlNUVVZXWFlaY2RlZmdoaWpzdHV2d3h5eoKDhIWGh4iJipKTlJWWl5iZmqKjpKWmp6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uLj5OXm5+jp6vLz9PX29/j5+v/aAAwDAQACEQMRAD8A/VKiiigAooooAMYoxRRQAUYxRQKACkxS0YoAMUUUUAGKMUYooAKMUUUAGKKKKADFFFFABRRRQAUUYpKAFooooAKM1ieLvF+leCNFm1TWLpLS0j7sfmduyqO5PpXyH8S/2qfEXim4ltvD7toOmAkK8Z/0iQepb+H6D86APsXVvEmlaEu7UdStLEetzOsefzNYVv8AF7wTdTLFF4r0hpGOAv2tBn9a/Oy7vbi/nee6nkuJn5aSVyzE/U1DQB+oFjqFrqcCzWlzDdQt0kgkDqfxFWM1+ZWg+J9W8L3i3Wkajc6dcD+O3kK5+o6H8a+j/hJ+1m8k0Gl+M1X5iETVIl24PT94o4/EflQB9TUVFa3MV5bxzwyLLDIoZHRsqwPIIPepaACiik70ALRRRQAUUCigAFFFFABRRQaADNV9Rv7fS7G4u7qVYLeCNpJJHOAqgZJqxXhH7XnjCTQfANvpNu5SfVpvLfaefKXlvzO0fnQB83/Gf4r3vxS8UyXDSPHpNsxjsrbPCrn7xH949z9K8+ozzRQAUUUUAHSg80UUAfRv7Lfxqm0vU4PCGsz77C4O2xlkP+pkPOzPo3b3+tfXQOea/Ly3uJLO4jnhcxyxMHR1OCrDkEV+jvwz8VL418B6LrOR5lzbqZRnpIOH/UGgDp6DRR2oAKKKKADpRRRQAfjRQKKACjHvRRQAV8i/trXEjeK/DsBYmJbJ3C+hMhBP/jo/Kvrqvlb9tjRHF14a1dVYoUltXbPAIIZf5t+VAHzBRRRQAYo7UYoNABjNGKKKAAV9wfsk3Ulx8IYEkbKw3s8aDHRchsfmxr4fNfeP7MWhtovwf0gurK940l2Qc9GbA/QCgD1eiig0AFFBooAKKKKACiiigAoopPyoAWuC+Nvw/wD+Fj/D+/0yIf6agFxat/00XkD8RkfjXe0hHFAH5eTwSWs8kMyNHLGxR0cYKkcEEetR19Z/tGfs8Ta7PP4p8M2++9IL3tjGADLj/lonq3qO/wBa+TpI3hkaORWSRTtZWGCCOoNADaKKKACijNafhvw1qXi3V4NN0m0kvLyZgqog4HuT2HuaANH4deC7r4g+MNO0S1Vv38gMrgcRxDl2P0H64r9GtK0+HSNNtbG2Ty7e2iWGNfRVGB/KvOPgX8F7f4U6GWnKXOu3YBurlRwg7Rqf7o/U16jQAUE0UUAFFIaWgAopMUUALQKKKACg80UUAGaRnCKSSAAMkmsPxj410nwJosuqaxdra2qcDPLO3ZVHUmvjH4s/tG698Q5prSxkfSNDPyi3ibEko/22H8hxQB9PeKP2ivA/hXVY9PuNVFzMW2yGzQypF/vMOPwGTWb4n+FXw/8Ajhaf2naTwNdOONQ02Rd5/wB9eh/EZr4W/nV3Sta1DQ7gT6dfXFjMDnfbyFD+lAHu/iL9jXxHZl30fVLLUo8/LHNmF8fqP17Vz/8AwyZ8QSRm0sQPX7YtZelftLfEPSgoGum6RQRi6hSTP1OM/rWmf2sviGf+Xyy/8A0oA7jwv+xhctMkniHXY0iHLQWCEsfbe3T8q9YS5+HX7PmkPEkttp8pXLKG827m+v8AEensK+S9b+PXj3xBE0V14jukiYEFLcLCCD2+UCuEuLmW7maWeV5pW6vIxZj9SaAP0Q8CfF3wv8Q4v+JRqcb3GMtaTfJKv/AT1/CuzBr8vbW7msrhJ7eV4JkO5JI2Ksp9QRX0p8Gf2qp7WW30fxlIZoWOyPVONyZ6eZ6j360AfV9BqG0u4b62int5UnglUOkkZ3KwPQgjrUxNACUtFGaAEopaKACiigUAFY3i7xVp/gvw/eavqcwhtLZNx55Y9lHqSeBWya+J/wBqL4qv4w8WNoNjMTpOlOY22niWb+I8dQOg+hoA4T4q/FPVfin4ge9vXMNnGSLWzVvkiX+pPc1xXSiigAooooAKKKDQAUUd6KADrRRRQB7t+zl8dpvBepweHtanaTQbl9kMjnJtXJ4x/snuO3Wvs+ORZEVlIZWGQQcgivy6r7K/ZU+Kr+KfD7+HNRl36jpiAwux5lg6Ae5Xp9MUAe/GikzxS0AJiilooAKKKM0AcP8AGjxsfAPw61bVY3CXXl+Tbf8AXV+F/Lk/hX54yO0sjO7FnYlmY9Se5r9Cfjf8PpfiV8Pr7SLZ1S+VluLYucKZFzgH6gkfjXwZq/g/XNBvpLPUNJvLW5jJDRvC3rjIOMEcdRxQBj0Zq3/ZF/8A8+Nx/wB+W/wo/se//wCfG5/78t/hQBU60Vb/ALHv/wDnyuf+/Lf4Uf2Rf/8APlc/9+W/woAqUVb/ALHv/wDnyuf+/Lf4Uf2Rf/8APlc/9+W/woAqYoq5/ZF//wA+Vz/35b/Ck/se/wD+fK5/78t/hQBUoxVv+yL/AP58rn/vy3+FL/ZF/wD8+Vz/AN+W/wAKAKddR8MvGMvgPxzpOtRsQkEwEwHO6JuHH5E1hf2Pf/8APlc/9+W/wrsPht8IPEHxD123tYLC4t7DePtF7LGVjjTPPJHJx2oA/QmCVZ4I5EO5HUMp9QelSVBY2qWFlb20Q2xQxrGgA6ADA/lU9ABiikooAWijvRQAGkwB0paKAEpcCiigBKMUHrS9qADig0CigBMUvFFFAAfpSYpTRQAcUhA9OaWg0AGMUUlL3oAKKOtFAAKKKKACjqKKKAA0UUUAGKKKKACiiigAo9KKKACiiigBKO1FFAC4ooooAKKKKAP/2Q==";
     }, function (errorObject) {
         console.log("The read failed: " + errorObject.code);
     });
@@ -176,12 +190,33 @@ financeApp.controller('ViewEmployeeController', function ($scope, $firebaseObjec
 financeApp.controller('AddNewClientController', function ($scope, $firebaseObject, $location, $timeout) {
     var rootRef = new Firebase(ROOTREF);
     var IdRef = rootRef.child("IDs/Client");
+    var CenterDdlRef = rootRef.child("Center");
+    var EmployeeDdlRef = rootRef.child("Employee");
+
+    CenterDdlRef.on("value", function (data) {
+        $scope.Centers = data.val();
+    }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+    });
+
+    EmployeeDdlRef.on("value", function (data) {
+        $scope.Employees = data.val();
+    }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+    });
+
     $scope.AddRec = function () {
         if (!formAdd.$invalid) {
             IdRef.once("value", function (snapshot) {
                 $scope.Client.ID = snapshot.val();
                 var key = rootRef.child('Client').child(snapshot.val());
                 key.set($scope.Client);
+
+                // Pushing Encoded Image
+                var Imgkey = rootRef.child('ClientPics').child(snapshot.val());
+                if ($scope.ClientPic != null)
+                    Imgkey.set($scope.ClientPic);
+
                 IdRef.set(parseInt($scope.Client.ID) + 1);
                 $scope.Client = null;
                 $timeout(function () {
@@ -192,14 +227,53 @@ financeApp.controller('AddNewClientController', function ($scope, $firebaseObjec
             });
         }
     }
+
+    $scope.GetFile = function (f) {
+        var reader = new FileReader();
+        reader.onload = (function (theFile) {
+            return function (e) {
+                var binaryData = e.target.result;
+                $scope.ClientPic = window.btoa(binaryData);
+                document.getElementById('base64').src = "data:image/png;base64, " + $scope.ClientPic;
+            };
+        })(f);
+        reader.readAsBinaryString(f);
+    }
+
 });
 
-financeApp.controller('UpdateClientController', function ($scope, $firebaseObject) {
+financeApp.controller('UpdateClientController', function ($scope, $firebaseObject,$location) {
     var rootRef = new Firebase(ROOTREF);
+    var CenterDdlRef = rootRef.child("Center");
+    var EmployeeDdlRef = rootRef.child("Employee");
+
+    CenterDdlRef.on("value", function (data) {
+        $scope.Centers = data.val();
+    }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+    }); var CenterDdlRef = rootRef.child("Center");
+
+    EmployeeDdlRef.on("value", function (data) {
+        $scope.Employees = data.val();
+    }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+    });
+
     var recordRef = rootRef.child("Client/" + $location.search().ID);
     recordRef.on("value", function (data) {
         console.log(data.val());
         $scope.Client = data.val();
+    }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+    });
+
+    var imageRef = rootRef.child("ClientPics/" + $location.search().ID);
+    imageRef.on("value", function (data) {
+        $scope.ClientPic = data.val();
+        if ($scope.ClientPic != null)
+            document.getElementById('base64').src = "data:image/png;base64, " + $scope.ClientPic;
+        else
+            document.getElementById('base64').src = "data:image/png;base64, " + "/9j/4AAQSkZJRgABAQAAAQABAAD//gA7Q1JFQVRPUjogZ2QtanBlZyB2MS4wICh1c2luZyBJSkcgSlBFRyB2NjIpLCBxdWFsaXR5ID0gOTAK/9sAQwADAgIDAgIDAwMDBAMDBAUIBQUEBAUKBwcGCAwKDAwLCgsLDQ4SEA0OEQ4LCxAWEBETFBUVFQwPFxgWFBgSFBUU/9sAQwEDBAQFBAUJBQUJFA0LDRQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQU/8AAEQgAlgCWAwEiAAIRAQMRAf/EAB8AAAEFAQEBAQEBAAAAAAAAAAABAgMEBQYHCAkKC//EALUQAAIBAwMCBAMFBQQEAAABfQECAwAEEQUSITFBBhNRYQcicRQygZGhCCNCscEVUtHwJDNicoIJChYXGBkaJSYnKCkqNDU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6g4SFhoeIiYqSk5SVlpeYmZqio6Slpqeoqaqys7S1tre4ubrCw8TFxsfIycrS09TV1tfY2drh4uPk5ebn6Onq8fLz9PX29/j5+v/EAB8BAAMBAQEBAQEBAQEAAAAAAAABAgMEBQYHCAkKC//EALURAAIBAgQEAwQHBQQEAAECdwABAgMRBAUhMQYSQVEHYXETIjKBCBRCkaGxwQkjM1LwFWJy0QoWJDThJfEXGBkaJicoKSo1Njc4OTpDREVGR0hJSlNUVVZXWFlaY2RlZmdoaWpzdHV2d3h5eoKDhIWGh4iJipKTlJWWl5iZmqKjpKWmp6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uLj5OXm5+jp6vLz9PX29/j5+v/aAAwDAQACEQMRAD8A/VKiiigAooooAMYoxRRQAUYxRQKACkxS0YoAMUUUUAGKMUYooAKMUUUAGKKKKADFFFFABRRRQAUUYpKAFooooAKM1ieLvF+leCNFm1TWLpLS0j7sfmduyqO5PpXyH8S/2qfEXim4ltvD7toOmAkK8Z/0iQepb+H6D86APsXVvEmlaEu7UdStLEetzOsefzNYVv8AF7wTdTLFF4r0hpGOAv2tBn9a/Oy7vbi/nee6nkuJn5aSVyzE/U1DQB+oFjqFrqcCzWlzDdQt0kgkDqfxFWM1+ZWg+J9W8L3i3Wkajc6dcD+O3kK5+o6H8a+j/hJ+1m8k0Gl+M1X5iETVIl24PT94o4/EflQB9TUVFa3MV5bxzwyLLDIoZHRsqwPIIPepaACiik70ALRRRQAUUCigAFFFFABRRQaADNV9Rv7fS7G4u7qVYLeCNpJJHOAqgZJqxXhH7XnjCTQfANvpNu5SfVpvLfaefKXlvzO0fnQB83/Gf4r3vxS8UyXDSPHpNsxjsrbPCrn7xH949z9K8+ozzRQAUUUUAHSg80UUAfRv7Lfxqm0vU4PCGsz77C4O2xlkP+pkPOzPo3b3+tfXQOea/Ly3uJLO4jnhcxyxMHR1OCrDkEV+jvwz8VL418B6LrOR5lzbqZRnpIOH/UGgDp6DRR2oAKKKKADpRRRQAfjRQKKACjHvRRQAV8i/trXEjeK/DsBYmJbJ3C+hMhBP/jo/Kvrqvlb9tjRHF14a1dVYoUltXbPAIIZf5t+VAHzBRRRQAYo7UYoNABjNGKKKAAV9wfsk3Ulx8IYEkbKw3s8aDHRchsfmxr4fNfeP7MWhtovwf0gurK940l2Qc9GbA/QCgD1eiig0AFFBooAKKKKACiiigAoopPyoAWuC+Nvw/wD+Fj/D+/0yIf6agFxat/00XkD8RkfjXe0hHFAH5eTwSWs8kMyNHLGxR0cYKkcEEetR19Z/tGfs8Ta7PP4p8M2++9IL3tjGADLj/lonq3qO/wBa+TpI3hkaORWSRTtZWGCCOoNADaKKKACijNafhvw1qXi3V4NN0m0kvLyZgqog4HuT2HuaANH4deC7r4g+MNO0S1Vv38gMrgcRxDl2P0H64r9GtK0+HSNNtbG2Ty7e2iWGNfRVGB/KvOPgX8F7f4U6GWnKXOu3YBurlRwg7Rqf7o/U16jQAUE0UUAFFIaWgAopMUUALQKKKACg80UUAGaRnCKSSAAMkmsPxj410nwJosuqaxdra2qcDPLO3ZVHUmvjH4s/tG698Q5prSxkfSNDPyi3ibEko/22H8hxQB9PeKP2ivA/hXVY9PuNVFzMW2yGzQypF/vMOPwGTWb4n+FXw/8Ajhaf2naTwNdOONQ02Rd5/wB9eh/EZr4W/nV3Sta1DQ7gT6dfXFjMDnfbyFD+lAHu/iL9jXxHZl30fVLLUo8/LHNmF8fqP17Vz/8AwyZ8QSRm0sQPX7YtZelftLfEPSgoGum6RQRi6hSTP1OM/rWmf2sviGf+Xyy/8A0oA7jwv+xhctMkniHXY0iHLQWCEsfbe3T8q9YS5+HX7PmkPEkttp8pXLKG827m+v8AEensK+S9b+PXj3xBE0V14jukiYEFLcLCCD2+UCuEuLmW7maWeV5pW6vIxZj9SaAP0Q8CfF3wv8Q4v+JRqcb3GMtaTfJKv/AT1/CuzBr8vbW7msrhJ7eV4JkO5JI2Ksp9QRX0p8Gf2qp7WW30fxlIZoWOyPVONyZ6eZ6j360AfV9BqG0u4b62int5UnglUOkkZ3KwPQgjrUxNACUtFGaAEopaKACiigUAFY3i7xVp/gvw/eavqcwhtLZNx55Y9lHqSeBWya+J/wBqL4qv4w8WNoNjMTpOlOY22niWb+I8dQOg+hoA4T4q/FPVfin4ge9vXMNnGSLWzVvkiX+pPc1xXSiigAooooAKKKDQAUUd6KADrRRRQB7t+zl8dpvBepweHtanaTQbl9kMjnJtXJ4x/snuO3Wvs+ORZEVlIZWGQQcgivy6r7K/ZU+Kr+KfD7+HNRl36jpiAwux5lg6Ae5Xp9MUAe/GikzxS0AJiilooAKKKM0AcP8AGjxsfAPw61bVY3CXXl+Tbf8AXV+F/Lk/hX54yO0sjO7FnYlmY9Se5r9Cfjf8PpfiV8Pr7SLZ1S+VluLYucKZFzgH6gkfjXwZq/g/XNBvpLPUNJvLW5jJDRvC3rjIOMEcdRxQBj0Zq3/ZF/8A8+Nx/wB+W/wo/se//wCfG5/78t/hQBU60Vb/ALHv/wDnyuf+/Lf4Uf2Rf/8APlc/9+W/woAqUVb/ALHv/wDnyuf+/Lf4Uf2Rf/8APlc/9+W/woAqYoq5/ZF//wA+Vz/35b/Ck/se/wD+fK5/78t/hQBUoxVv+yL/AP58rn/vy3+FL/ZF/wD8+Vz/AN+W/wAKAKddR8MvGMvgPxzpOtRsQkEwEwHO6JuHH5E1hf2Pf/8APlc/9+W/wrsPht8IPEHxD123tYLC4t7DePtF7LGVjjTPPJHJx2oA/QmCVZ4I5EO5HUMp9QelSVBY2qWFlb20Q2xQxrGgA6ADA/lU9ABiikooAWijvRQAGkwB0paKAEpcCiigBKMUHrS9qADig0CigBMUvFFFAAfpSYpTRQAcUhA9OaWg0AGMUUlL3oAKKOtFAAKKKKACjqKKKAA0UUUAGKKKKACiiigAo9KKKACiiigBKO1FFAC4ooooAKKKKAP/2Q==";
     }, function (errorObject) {
         console.log("The read failed: " + errorObject.code);
     });
@@ -252,6 +326,7 @@ financeApp.controller('UpdateVendorController', function ($scope, $firebaseObjec
 financeApp.controller('NewCenterController', function ($scope, $firebaseObject, $location, $timeout) {
     var rootRef = new Firebase(ROOTREF);
     var IdRef = rootRef.child("IDs/Center");
+
     $scope.AddRec = function () {
         if (!formAdd.$invalid) {
             IdRef.once("value", function (snapshot) {
